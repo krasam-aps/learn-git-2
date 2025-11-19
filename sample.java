841387b7679551f0ce782e05335b1f3ec9f72158
krasam-aps <krishrasam@apstrainingacademy.org>
2025-11-18 20:28:47 -0600

    camelCaseKR: Added line 5

--- sample.java
+++ sample.java
@@ -1,6 +1,7 @@
 public  class helloworld{
   public static void main(Sting[] args)
   {
-    System.out.println("Hello, World! This is KRISH!!!")
+    System.out.println("Hello, World! This is KRISH!!!");
+    System.out.println("I like LEGOs and Hotwheels.");
       }
 }
