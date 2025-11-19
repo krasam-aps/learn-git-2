10fa5561ca206b277f75cb4c9c7d13406bad8b8b
Krish Rasam <krishrasam@apstrainingacademy.org>
2025-11-18 20:42:42 -0600

    camelCaseKR: Commiting my changes to fix errors

--- sample.java
+++ sample.java
@@ -1,7 +1,7 @@
-public  class helloworld{
-  public static void main(Sting[] args)
-  {
-    System.out.println("Hello, World! This is KRISH!!!");
-    System.out.println("I like LEGOs and Hotwheels.");
-      }
+public class sample {
+    public static void main(String[] args) {
+        System.out.println("Hello, World! This is KRISH!!!");
+        System.out.println("I like LEGOs and Hotwheels.");
+        System.out.println("Fixed class name to match file name, and reformated the code");
+    }
 }
